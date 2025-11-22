1100a6ea132499ccf1f9cb6e33e224fcc670dd13
Emmanuel B <blancocemmanuel@gmail.com>
2025-11-22 20:12:52 +1100

    comments

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import Card from './Card'
 
 function App () {
  const allComments = comments.map(comment => {
-    return <Card commentObject={comment} key={comment.comment} />
+    return <Card commentObject={comment} key={comment.comment} /> //allComments value  is 3 Card instances, commentObject={comment}, each representig one object// Key={comment.comment} is only for React to help it track each Card in the list and is not passed to the Card component itself. 
   })
 return allComments;
 }
